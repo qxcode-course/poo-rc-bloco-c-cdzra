bf928ab5c6d20e2d376956dd60ce145557105e11
Carlos Eduardo <cadus824@gmail.com>
2025-11-16 15:30:40 -0300

    quase la

--- myrep/poo/pula-pula/py/draft.py
+++ myrep/poo/pula-pula/py/draft.py
@@ -17,3 +17,25 @@ class Crianca:
 
     def __str__(self):
         return f"{Crianca.getNome}:{Crianca.getIdade}"
+    
+class Trampolim:
+    def __init__(self):
+        self.brincando: list[Crianca] = []
+        self.esperando: list[Crianca] = []
+
+    def removeFromList(self, nome: str, listC: list[Crianca]) -> Crianca | None:
+        for i, crianca in enumerate(listC):
+            if crianca.getNome() == nome:
+                return listC.pop(i)
+            return None
+        
+    def removerCrianca(self, nome: str) -> Crianca | None:
+        crianca = self.removeFromList(nome, self.esperando)
+        if crianca is not None:
+            return crianca
+        return self.removeFromList(nome, self.brincando)
+    
+    def arrive(self, crianca: Crianca):
+        self.esperando.insert(0, crianca)
+        
+
