6e9882ab252075f14c9f982d92dc69a04a7a4503
Carlos Eduardo <cadus824@gmail.com>
2025-11-16 16:06:15 -0300

    pula boi pula cavalo

--- myrep/poo/pula-pula/py/draft.py
+++ myrep/poo/pula-pula/py/draft.py
@@ -16,7 +16,7 @@ class Crianca:
         self.__idade = idade
 
     def __str__(self):
-        return f"{Crianca.getNome}:{Crianca.getIdade}"
+        return f"{self.__nome}:{self.__idade}"
     
 class Trampolim:
     def __init__(self):
@@ -27,7 +27,7 @@ class Trampolim:
         for i, crianca in enumerate(listC):
             if crianca.getNome() == nome:
                 return listC.pop(i)
-            return None
+        return None
         
     def removerCrianca(self, nome: str) -> Crianca | None:
         crianca = self.removeFromList(nome, self.esperando)
@@ -43,11 +43,36 @@ class Trampolim:
         self.brincando.insert(0, crianca)
     
     def sair(self):
-        crianca = self.esperando.pop(-1)
-        self.brincando.insert(0, crianca)
+        if len(self.brincando) > 0:
+            crianca = self.brincando.pop(-1)
+            self.esperando.insert(0, crianca)
 
     def __str__(self):
         espera = ", ".join(str(x) for x in self.esperando)
         brincando = ", ".join(str(x) for x in self.brincando)
         return f"[{espera}] => [{brincando}]"
-
+    
+def main():
+    trampolim = Trampolim()
+    while True:
+        line = input()
+        print("$" + line)
+        args: list[str] = line.split()
+        if args[0] == "end":
+            break
+        elif args[0] == "show":
+            print(trampolim)
+        elif args[0] == "arrive":
+            nome = args[1]
+            idade = int(args[2])
+            trampolim.chegar(Crianca(nome, idade))
+        elif args[0] == "enter":
+            trampolim.entrar()
+        elif args[0] == "leave":
+            trampolim.sair()
+        elif args[0] == "remove":
+            nome = args[1]
+            crianca = trampolim.removerCrianca(nome)
+            if crianca is None:
+                print(f"fail: {nome} nao esta no pula-pula")
+main()
