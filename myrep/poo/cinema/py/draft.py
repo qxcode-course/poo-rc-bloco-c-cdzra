c07facfa53086012546d7617a43b22a2223277fc
Carlos Eduardo <cadus824@gmail.com>
2025-11-18 14:13:07 -0300

    quase la

--- myrep/poo/cinema/py/draft.py
+++ myrep/poo/cinema/py/draft.py
@@ -17,3 +17,19 @@ class Cliente:
     
     def __str__(self):
         return f"{self.__id}:{self.__fone}"
+    
+class Cinema:
+    def __init__(self, capacidade: int):
+        self.__acentos = list[Cliente | None] = [None] * capacidade
+        self.__capacidade = capacidade
+
+    def busca(self, id: str) -> int:
+        for i, cliente in enumerate(self.__acentos):
+            if cliente is not None and cliente.getId() == id:
+                return i
+        return -1
+
+    def verificarIndex(self, index: int) -> bool:
+        return 0 <= index < self.__capacidade
+    
+    
